dd8b9c562a6cdf9801083aaa4761ffcddf5247df
Nabil <rousani@gmail.com>
2025-02-06 09:34:12 -0800

    done with hide/show

--- w5-js-part-ii/2/code.js
+++ w5-js-part-ii/2/code.js
@@ -1,8 +1,13 @@
 function main() {
-  // $("#ul1").append("<li> item x </li>")
-  // $("#ul1").prepend("<li> item x </li>")
-  // $("#ul1").before("<li> item x </li>")
-  // $("#ul1").aft/er("<li> item x </li>")
+  // $(".pppp").remove()
+  // $(".pppp").hide()
+  // $(".pppp").empty() // same as   $(".pppp").html("")
+
+  $(".pppp").hide()
+  // $(".pppp").show()
+  $(".pppp").toggle()
+  $(".pppp").toggle()
+
 }
 
 

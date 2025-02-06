dfa69fa7b1e779310abbaecb5fab4642730fe8af
Nabil <rousani@gmail.com>
2025-02-06 09:52:30 -0800

    done with Traversing

--- w5-js-part-ii/2/code.js
+++ w5-js-part-ii/2/code.js
@@ -1,23 +1,13 @@
 function main() {
-  // $("#p1").addClass("blue-background")
-  // $("#p1").addClass("bg-cyan-500")
-  // $("#p1").addClass("text-slate-300")
-  // $("#p1").removeClass("bg-cyan-500")
-  // $("#p1").css("background-color", "tomato")
-  // $("#p1").addClass("bg-cyan-500 text-slate-300")
-  // $("#p1")
-  //   .addClass("bg-cyan-500")
-  //   .addClass("text-slate-300")
 
-  $("#p1").html(
-    `
-      <span class="bg-cyan-500"> 
-        Some bluish text
-      </span>
-      other tes 
-    `
-  )
-
+  // console.log($("#item2").html());
+  // console.log($("#item2").parent().html());
+  // console.log($("#item2").parent().children());
+  // console.log($("#item2").siblings());
+  // console.log($("#item2").next().html());
+  // console.log($("#item2").nextAll());
+  // console.log($("#item2").nextUntil("#itemx"));
+  console.log($("ul").find("#itemx"));
 }
 
 

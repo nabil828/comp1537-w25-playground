9683c6c8e36073a68b746f0dc67e648f3e28ee64
Nabil <rousani@gmail.com>
2025-02-06 09:10:43 -0800

    edits

--- w5-js-part-ii/1/code.js
+++ w5-js-part-ii/1/code.js
@@ -1,4 +1,12 @@
-// // Step 1: Define calculate function
+
+function addFunction() {
+  x = $("#op1").val() // read
+  y = $("#op2").val() //read
+
+  z = calculate(Number(x), "+", Number(y))
+  $("#resultParagraph").html(z) // write
+}
+
 function calculate(num1, operator, num2) {
   // TODO
   // Step 2: Switch on the operator
@@ -12,17 +20,15 @@ function calculate(num1, operator, num2) {
 }
 
 
-function addFunction() {
-  x = $("#op1").val()
-  y = $("#op2").val()
-
-  z = calculate(Number(x), "+", Number(y))
-  $("#result").html(z)
-}
-
 
 function setup() {
-  $("#addBtn").click(addFunction)
+  $("#op1").val(1) // write
+  $("#op2").val(2) // write
+  $("#resultParagraph").html("<b>THe is the result Para</b>") // write
+
+  $("#addBtn").click(addFunction) // addFunction is a callback
+  // callback is a function that is gonna be called in future
+  // addFunction is an EventHandler 
 }
 
-$(document).ready(setup) 
+jQuery(document).ready(setup) 

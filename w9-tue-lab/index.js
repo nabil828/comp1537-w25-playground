e577b82a8e40fd2f72ab677916020f4c45af8fcb
Nabil <rousani@gmail.com>
2025-03-06 08:08:50 -0800

    w9

---
+++ w9-tue-lab/index.js
@@ -0,0 +1,73 @@
+current_page = 1
+
+
+function process_response(response) {
+  console.log(response);
+  for (let i = 0; i < 20 * current_page; i++) {
+    document.getElementById("result").innerHTML +=
+      `
+        <div class="card basis-1/4">
+          <h1>
+              ${response.data.movies[i].title}
+          </h1>
+
+
+          <div class="relative">
+            <img src="${response.data.movies[i].large_cover_image}" alt="" />
+            <div class="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+                ${response.data.movies[i].rating}
+                <svg  xmlns="http://www.w3.org/2000/svg" class="text-green-400"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-star"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" /></svg>
+            </div>
+          </div>
+        </div>
+      `
+  }
+
+  // document.getElementById("result").innerHTML +=
+  //   `
+  //   <div>
+  //     <button id="viewMoreBtn" class="bg-green-500 p-4">
+  //     View More
+  //     </button>
+  //   </div>
+  // `
+  x = document.createElement("div")
+  x.id = "viewMoreBtn"
+  x.classList = "bg-green-500 p-4"
+  x.innerHTML = "View more!"
+  // x.textContent = "test this out"
+
+  document.getElementById("result").appendChild(x)
+
+  document.getElementById(`viewMoreBtn`).addEventListener("click", () => {
+    current_page++
+    fetch(`https://yts.mx/api/v2/list_movies.json?genre=animation&minimum_rating=8&page=${current_page}`)
+    .then(r=>r.json())
+    .then(
+      (response)=>{
+        process_response(response)
+      }
+    )
+    
+  })
+
+}
+
+setup = function () {
+  fetch(`https://yts.mx/api/v2/list_movies.json?genre=animation&minimum_rating=8`)
+    .then(resp => resp.json())
+    .then(respJSON => {
+      console.log(respJSON);
+      process_response(respJSON)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+
+}
+
+// $(document).ready(setup)
+setup()
+
+
---
+++ w9-vanilla-js/readme.md
@@ -0,0 +1,14 @@
+## Table of Contents
+
+1. [Introduction](1.intro.md)
+2. [Syntax](2.syntax.md)
+3. [Selection](3.selection.md)
+4. [Modifying Content](4.modifying.content.md)
+5. [Events](5.events.md)
+6. [Creating, Adding, and Removing Elements](6.create.add.remove.elements.md)
+7. [Traversing](7.traversing.md)
+8. [Fetch API](8.fetch-api.md)
+
+## Additional Files
+
+- [`list.todo`](list.todo) – A list of tasks.
